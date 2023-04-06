2f5af373818f21fa01be1cf59aeccc02597dbada
Tiago Oliveira <pliveiratiago7890@gmail.com>
2023-04-05 22:17:09 -0300

    Requisito 10

--- challenges/desafio10.sql
+++ challenges/desafio10.sql
@@ -1,13 +1,36 @@
--- Descomente e altere as linhas abaixo:
+CREATE TABLE SpotifyClone.favorites  (
+ 
+  user_id INT NOT NULL,
+  song_id INT NOT NULL,
+  PRIMARY KEY (user_id, song_id),
+  FOREIGN KEY (user_id) REFERENCES user(user_id)
 
--- CREATE TABLE SpotifyClone.tabela(
---     coluna1 tipo restricoes,
---     coluna2 tipo restricoes,
---     colunaN tipo restricoes,
--- ) engine = InnoDB;
+/*ON DELETE CASCADE, isso significa que quando um registro da tabela principal é excluído, 
+todos os registros relacionados da tabela referenciada serão automaticamente excluídos também, 
+sem a necessidade de executar uma instrução DELETE separada. */
 
--- INSERT INTO SpotifyClone.tabela (coluna1, coluna2)
--- VALUES
---   ('exemplo de dados 1', 'exemplo de dados A'),
---   ('exemplo de dados 2', 'exemplo de dados B'),
---   ('exemplo de dados 3', 'exemplo de dados C');
+    ON DELETE CASCADE,
+  FOREIGN KEY (song_id) REFERENCES song(song_id),
+
+  /* UNIQUE_SONG é uma restrição de índice único que impede a 
+  inserção de registros duplicados na tabela favorite_songs. */.
+
+  CONSTRAINT unique_song UNIQUE (user_id, song_id)
+) engine = InnoDB;
+
+
+
+INSERT INTO SpotifyClone.favorites (user_id, song_id) VALUES
+	('1', '3'),
+	('1', '6'),
+	('1', '10'),
+	('2', '4'),
+	('3', '1'),
+	('3', '3'),
+	('4', '7'),
+	('4', '4'),
+	('5', '10'),
+	('5', '2'),
+	('8', '4'),
+	('9', '7'),
+	('10', '3');
